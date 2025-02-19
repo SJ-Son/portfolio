188abc06b685ceda25a6603965c630eab51e2df1
sungjun son <o_nj_u@naver.com>
2025-02-19 12:22:58 +0900

    20250219 Admin 뷰 개발 - 테이블 페이지(Table)

--- src/main/kotlin/com/sungjun/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
+++ src/main/kotlin/com/sungjun/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
@@ -1,8 +1,10 @@
 package com.sungjun.portfolio.admin.interceptor
 
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@Configuration
 class AdminInterceptorConfiguration(
     val adminInterceptor: AdminInterceptor
 ) : WebMvcConfigurer {
